3a921223b4cdb804f31fa9c38d43e1da58aa39c7
Diego Buzo <90202864+dlkax@users.noreply.github.com>
2025-11-11 00:53:06 +0000

    deploy

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import logging
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+CORS(app, resources={
+    r"/*": {"origins": ["https://financemind.net", "https://www.financemind.net"]}
+})
+
 
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
